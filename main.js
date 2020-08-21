86537670fe2d14acd13985b5e0a79eda19c574d6
anhpnh <anhpnh@gmail.com>
2020-08-21 09:03:27 +0700

    bai3 - thuc hien cuoc goi video gua cac client

--- main.js
+++ main.js
@@ -9,10 +9,32 @@ function playStream(idVideoTag, stream) {
     video.play();
 }
 
-openStream()
-    .then(stream => playStream("localStream", stream));
+// openStream()
+//     .then(stream => playStream("localStream", stream));
 
 const peer = new Peer();
 peer.on('open', id => {
     $("#my-peer").append(id);
+});
+
+
+//Caller
+$("#btnCall").click(() => {
+    const id = $("#remoteID").val();
+    openStream()
+        .then(stream => {
+            playStream("localStream", stream);
+            const call = peer.call(id, stream);
+            call.on("stream", remoteStream => playStream("remoteStream", remoteStream));
+        });
+});
+
+//Answer
+peer.on("call", call => {
+    openStream()
+        .then(stream => {
+            call.answer(stream);
+            playStream("localStream", stream);
+            call.on("stream", remoteStream => playStream("remoteStream", remoteStream));
+        });
 });
